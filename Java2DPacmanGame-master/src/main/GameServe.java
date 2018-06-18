c87974558173da4f83f6f7ac76e1005de1d5ae27
Aluísio <aluisiomeloni@gmail.com>
2018-06-18 20:52:32 -0300

    Remoção de Classes
    Remoção de classes que não eram necessárias ou protótipos de classes.

--- Java2DPacmanGame-master/src/main/GameServe.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public interface GameServe extends java.rmi.Remote {
-    
-     public void top() throws java.rmi.RemoteException;	
-     public boolean solicitarConexao(int n) throws java.rmi.RemoteException;
-     
-}
